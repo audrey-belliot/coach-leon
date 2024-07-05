15577c222a9f391cf4b648203323e537e549253e
audrey belliot <belliotaudrey@gmail.com>
2024-07-05 17:45:50 +0200

    v1 des graphes

--- app/controllers/health_logs_controller.rb
+++ app/controllers/health_logs_controller.rb
@@ -1,2 +1,20 @@
 class HealthLogsController < ApplicationController
+
+
+  def index
+    @healthlogs = HealthLog.all.to_a
+    @weights = []
+    @dates = []
+    @activities = []
+    @meals = []
+    @mood = []
+    @healthlogs.each do |healthlog|
+      @weights << healthlog.weight
+      @dates << healthlog.date.strftime("%b %Y")
+      @activities << healthlog.rating_activity * 10
+      @meals << healthlog.rating_nutrition * 10
+      @mood << healthlog.rating_mood * 10
+    end
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+HealthLog.destroy_all
+
+
+HealthLog.create(date: DateTime.new(2024,1,1), weight: 100.0, measurement: 162, rating_nutrition: 2, rating_activity: 3, rating_mood: 3, water: 0.5 , user_id: 1)
+HealthLog.create(date: DateTime.new(2024,2,1), weight: 98.0, measurement: 162, rating_nutrition: 3, rating_activity: 5, rating_mood: 4, water: 0.5 , user_id: 1)
+HealthLog.create(date: DateTime.new(2024,3,1), weight: 97.0, measurement: 162, rating_nutrition: 4, rating_activity: 6, rating_mood: 5, water: 0.5 , user_id: 1)
+HealthLog.create(date: DateTime.new(2024,4,1), weight: 92.0, measurement: 162, rating_nutrition: 5, rating_activity: 6, rating_mood: 6, water: 0.5 , user_id: 1)
