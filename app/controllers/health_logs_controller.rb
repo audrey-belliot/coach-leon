1706424a954e4924fbcc3e2076429c5de15dd329
Marie D HUYVETTER <marie_dhuyvetter@yahoo.fr>
2024-07-07 21:24:26 +0200

    health log new

--- app/controllers/health_logs_controller.rb
+++ app/controllers/health_logs_controller.rb
@@ -1,2 +1,21 @@
 class HealthLogsController < ApplicationController
+  def new
+    @health_log = HealthLog.new
+  end
+
+  def create
+    @health_log = HealthLog.new(health_log_params)
+    @health_log.user = current_user
+    if @health_log.save
+      redirect_to @health_log, notice: "Infos mises à jour"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def health_log_params
+    params.require(:health_log).permit(:date, :weight, :rating_nutrition, :rating_activity, :rating_mood, :water)
+  end
 end
