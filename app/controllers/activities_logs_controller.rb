bb9cb53d1e7117051a6ae17738f3d5d2a9f71036
audrey belliot <belliotaudrey@gmail.com>
2024-07-08 21:11:56 +0200

    add activity

--- app/controllers/activities_logs_controller.rb
+++ app/controllers/activities_logs_controller.rb
@@ -4,5 +4,22 @@ def new
   @activity_log = ActivitiesLog.new
 end
 
+def create
+
+  @activity_log = ActivitiesLog.new(activities_log_params)
+    @activity_log.user = current_user
+    if activity_log.save
+      redirect_to health_logs_path, notice: "Infos mises à jour"
+    else
+      render :new, status: :unprocessable_entity
+    end
+end
+
+private
+
+def activities_log_params
+  params.require(:activities_log).permit(:date, :comment, :picture, :activity_id)
+end
+
 
 end
