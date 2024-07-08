7a1da01b8d590fe715f002339ded7df0e71a023f
Marie D HUYVETTER <marie_dhuyvetter@yahoo.fr>
2024-07-08 22:21:57 +0200

    header navbar front

--- app/javascript/controllers/water_controller.js
+++ app/javascript/controllers/water_controller.js
@@ -7,28 +7,22 @@ export default class extends Controller {
   waterRise05() {
     this.water05Target.classList.add('fill');
     const waterValue = parseFloat(this.water05Target.getAttribute('value'));
-    console.log(`Water value: ${waterValue} liters`);
+    console.log(waterValue);
   }
 
   waterRise1() {
-    this.water05Target.classList.add('fill');
     this.water1Target.classList.add('fill');
     const waterValue = parseFloat(this.water1Target.getAttribute('value'));
     console.log(`Water value: ${waterValue} liters`);
   }
 
   waterRise15() {
-    this.water05Target.classList.add('fill');
-    this.water1Target.classList.add('fill');
     this.water15Target.classList.add('fill');
     const waterValue = parseFloat(this.water15Target.getAttribute('value'));
     console.log(`Water value: ${waterValue} liters`);
   }
 
   waterRise2() {
-    this.water05Target.classList.add('fill');
-    this.water1Target.classList.add('fill');
-    this.water15Target.classList.add('fill');
     this.water2Target.classList.add('fill');
     const waterValue = parseFloat(this.water2Target.getAttribute('value'));
     console.log(`Water value: ${waterValue} liters`);
