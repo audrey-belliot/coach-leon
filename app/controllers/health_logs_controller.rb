7689822efb590c42f0158c2ec4b24b8d48ce6c2d
audrey belliot <belliotaudrey@gmail.com>
2024-07-08 22:09:24 +0200

    add full date on chart

--- app/controllers/health_logs_controller.rb
+++ app/controllers/health_logs_controller.rb
@@ -13,7 +13,7 @@ class HealthLogsController < ApplicationController
     @mood = []
     @healthlogs.each do |healthlog|
       @weights << healthlog.weight
-      @dates << healthlog.date.strftime("%b %Y")
+      @dates << healthlog.date.strftime("%d/%m/%Y")
       @activities << healthlog.rating_activity * 10
       @meals << healthlog.rating_nutrition * 10
       @mood << healthlog.rating_mood * 10
