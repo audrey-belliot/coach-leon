fca965afd8a86f58c689619932c97f71f5474436
audrey belliot <belliotaudrey@gmail.com>
2024-07-09 16:47:48 +0200

    form recipes

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,8 @@ class ActivitiesController < ApplicationController
 
 def index
   @activities = Activity.all
+  @cardio = Activity.where(category: "Cardio")
+  @muscu = Activity.where(category: "Musculation")
 end
 
 end
