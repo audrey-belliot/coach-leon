1b1a431e29289c082a3fa4d1c8886dc28eef7492
Kevin Chatignon kevin.chatignon@gmail.com <kevin.chatignon@gmail.com>
2024-07-09 19:51:17 +0200

    first version of activity_logs/:id show

--- app/controllers/activities_logs_controller.rb
+++ app/controllers/activities_logs_controller.rb
@@ -3,4 +3,9 @@ class ActivitiesLogsController < ApplicationController
     @activities_logs = current_user.activities_logs
     @total = @activities_logs.count
   end
+
+  def show
+    @activity_log = ActivitiesLog.find(params[:id])
+    @activity_log_date = @activity_log.activity.created_at
+  end
 end
