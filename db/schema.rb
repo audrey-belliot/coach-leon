13d7235d9a9523c28e2c1c3f53c1caf10cb4b7a4
Valentin DUCRETTET <valentin.ducrettet@partagetabanque.fr>
2024-07-10 22:31:51 +0200

    toggle-home-ok

--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_154011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "firstname"
-    t.string "goal" 
+    t.string "goal"
     t.string "food_preferences"
     t.string "sport_preferences"
     t.date "birthdate"
