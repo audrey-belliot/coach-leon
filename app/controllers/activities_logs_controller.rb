9c8c67ab221e7b938a144f33d8f2aa5b9488e32f
Marie D HUYVETTER <marie_dhuyvetter@yahoo.fr>
2024-07-16 11:38:53 +0200

    strava activities logs

--- app/controllers/activities_logs_controller.rb
+++ app/controllers/activities_logs_controller.rb
@@ -1,14 +1,24 @@
 class ActivitiesLogsController < ApplicationController
   before_action :set_activity, only: %i[create new]
 
- def index
-  @activities_logs = current_user.activities_logs
-  @total = @activities_logs.count
-  @plan = current_user.plans.order(:start_date).last
+  def index
+    @activities_logs = current_user.activities_logs
+    @total = @activities_logs.count
+    @plan = current_user.plans.order(:start_date).last
 
-  athlete_client = Strava::Api::Client.new(access_token: session[:strava_access_token])
-  @activities = athlete_client.athlete_activities
-end
+    athlete_client = Strava::Api::Client.new(access_token: session[:strava_access_token])
+    @activities = athlete_client.athlete_activities
+
+    @activities.each do |strava|
+      # si l'activité strava n'existe pas dans la DB
+        # on cherche dans la DB les activities logs
+        # on regarde si l'activité strava correspond à une activity log
+      # si non : on créé une acivity log strava
+      if !ActivitiesLog.where(strava_id: strava.upload_id.to_s).exists?
+        ActivitiesLog.create!(date: strava.start_date, activity: Activity.last, user: current_user, comment: strava.name, duration: strava.elapsed_time.fdiv(60), strava_id: strava.upload_id.to_s)
+      end
+      end
+  end
 
   def show
     @activity_log = ActivitiesLog.find(params[:id])
