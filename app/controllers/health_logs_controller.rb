641feb53db03fdef627abd92ed6a7ac7fb79bc71
audrey belliot <belliotaudrey@gmail.com>
2024-07-17 23:38:38 +0200

    chart

--- app/controllers/health_logs_controller.rb
+++ app/controllers/health_logs_controller.rb
@@ -1,8 +1,15 @@
 class HealthLogsController < ApplicationController
 
   def index
-    start_date = params[:start_date]
-    end_date = params[:end_date]
+
+    if params[:commit].nil?
+      start_date = Date.today.beginning_of_month
+      end_date = Date.today
+    else
+      start_date = params[:start_date]
+      end_date = params[:end_date]
+    end
+
     @healthlogs = HealthLog.where(date: start_date..end_date, user: current_user).to_a
     @healthlogs = @healthlogs.sort_by{ |k| k["date"]}
     @allactivities = ActivitiesLog.where(date: start_date..end_date, user: current_user)
@@ -19,7 +26,6 @@ class HealthLogsController < ApplicationController
       @meals << healthlog.rating_nutrition * 10
       @mood << healthlog.rating_mood * 10
     end
-
 
   end
 
