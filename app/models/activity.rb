c4d86a5c7879dd70a4dfa9e282ef92782d09cd3a
audrey belliot <belliotaudrey@gmail.com>
2024-07-18 16:53:54 +0200

    strava

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,16 @@
 class Activity < ApplicationRecord
   has_many :activities_logs
   has_many :plans_activities
+
+  SPORTS_STRAVA = [
+    # { activity: Activity.find_by(activity: "Course à pied"), sport_type: "Run" },
+    # { activity: Activity.find_by(activity: 'Vélo de route'), sport_type: "Bike" },
+    # { activity: Activity.find_by(activity: 'Marche rapide'), sport_type: "Walk" },
+    # { activity: Activity.find_by(activity: 'Natation'), sport_type: "Swim" }
+    { activity: "Course à pied", sport_type: "Run" },
+    { activity: "Vélo de route", sport_type: "Ride" },
+    { activity: "Marche rapide", sport_type: "Walk" },
+    { activity: "Natation", sport_type: "Swim" }
+  ]
+
 end
