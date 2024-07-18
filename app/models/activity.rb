e2856b963b143ba19982ff9d9317519f4e73d518
Marie D HUYVETTER <marie_dhuyvetter@yahoo.fr>
2024-07-18 22:11:15 +0200

    merge

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,13 +3,6 @@ class Activity < ApplicationRecord
   has_many :plans_activities
 
   SPORTS_STRAVA = [
-<<<<<<< HEAD
-    { activity: Activity.where(activity: "Course à pied"), sport_type: "Run" },
-    { activity: Activity.where(activity: 'Vélo de route'), sport_type: "Bike" },
-    { activity: Activity.where(activity: 'Marche rapide'), sport_type: "Walk" },
-    { activity: Activity.where(activity: 'Natation'), sport_type: "Swim" }
-  ]
-=======
     # { activity: Activity.find_by(activity: "Course à pied"), sport_type: "Run" },
     # { activity: Activity.find_by(activity: 'Vélo de route'), sport_type: "Bike" },
     # { activity: Activity.find_by(activity: 'Marche rapide'), sport_type: "Walk" },
@@ -20,5 +13,4 @@ class Activity < ApplicationRecord
     { activity: "Natation", sport_type: "Swim" }
   ]
 
->>>>>>> 9a6acdf2069f6dd4866e1540ca177504409c1c73
 end
